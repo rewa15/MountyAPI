92931f537b9cb730f783e20ffe90a46c5763201e
Rewa Kawatra <rewa.kawatra@gmail.com>
2020-11-02 15:37:15 +0530

    Add more api's to app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const User = require('./models/user');
 const connectDB = require('./configurations/db');
 
 connectDB();
@@ -27,24 +28,38 @@ app.get("/get-users", function(req,res)
 // Add a new User
 app.post("/add-user",function(req,res)
 {
-  let newArticle= new Article({
-        title: req.body.title,
-        content: req.body.content
+  let today = new Date();
+  let newAddress = {};
+  newAddress.street = req.body.street;
+  newAddress.locality = req.body.locality;
+  newAddress.city = req.body.city;
+  newAddress.state = req.body.state;
+  newAddress.pincode = req.body.pincode;
+  newAddress.coordinatesType = req.body.coordinatesType;
+  newAddress.coordinates = req.body.coordinates;
+
+  let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        mobile: req.body.mobile,
+        createdAt: today,
+        address: newAddress
   });
+
   newUser.save(function(err) {
-        if(!err)
-        {
-          res.send("Sucess!!")
-        }else {
-          res.send(err);
-        }
-    });
+      if(!err)
+      {
+        res.send("Sucess!! Added new User.")
+      }else {
+        res.send(err);
+      }
+  });
 })
 
 // Update a specific user
 app.put("/update-user/:userNo",function(req,res)
 {
-  User.updateOne({mobile: req.params.userNo},{title: req.body.title, content: req.body.content},{overwrite: true}, function(err){
+  User.updateOne({mobile: req.params.userNo},{email: req.body.email},{overwrite: true}, function(err){
       if(!err)
       {
         res.send("Succesfully updated User!");
@@ -54,20 +69,20 @@ app.put("/update-user/:userNo",function(req,res)
   })
 })
 
-// Delete a specific user
+// Delete a specific user 
 app.delete("/delete-user/:userNo",function(req,res)
 {
   User.deleteOne({mobile: req.params.userNo},function(err){
     if(!err)
     {
-      res.send("Succesfully deleted corresponding Article!");
+      res.send("Succesfully deleted corresponding User!");
     }else {
       res.send(err);
     }
   });
 })
 
-// Get all Users sorted by CreatedAt timestamp 
+// Get all Users sorted by CreatedAt timestamp in ascending order
 app.get("/get-sorted-users", function(req,res)
 {
   User.find({}).sort('createdAt').exec(function(err, users) {
@@ -77,6 +92,25 @@ app.get("/get-sorted-users", function(req,res)
       res.send(err);
     }
   });
+})
+
+app.get("/get-nearest-users/:latitude/:longitude", function(req,res)
+{
+    // Setting Max Distance as 8 kilometers
+    let maxDistance = req.query.distance || 8;
+    // Convert the distance to radians
+    // the raduis of Earth is approximately 6371 kilometers
+    maxDistance /= 6371;
+
+    let coordsArr = [];
+    coordsArr[0] = req.params.longitude;
+    coordsArr[1] = req.params.latitude;
+    User.find({coordinates: {  $near: coordsArr, $maxDistance: maxDistance }}).exec(function(err, allUsers) {
+      if (err) {
+        res.send(err);
+      }
+      res.send(allUsers);
+    });
 })
 
 app.listen(3000, function() { console.log("Server started on port 3000"); });
